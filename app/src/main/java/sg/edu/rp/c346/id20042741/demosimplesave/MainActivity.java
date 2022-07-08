824b1560ee80f6121f47dacb227cd228858a9123
Yeow Heng <20042741@myrp.edu.sg>
2022-07-08 10:05:24 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20042741/demosimplesave/MainActivity.java
@@ -0,0 +1,16 @@
+package sg.edu.rp.c346.id20042741.demosimplesave;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    
+}
