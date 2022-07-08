ab59347d39b5f9b294162a83923e479462ddf9f7
Yeow Heng <20042741@myrp.edu.sg>
2022-07-08 11:01:56 +0800

    onPause and onResume completed

--- app/src/main/java/sg/edu/rp/c346/id20042741/demosimplesave/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20042741/demosimplesave/MainActivity.java
@@ -2,7 +2,9 @@ package sg.edu.rp.c346.id20042741.demosimplesave;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor prefEdit = prefs.edit();
+        prefEdit.putString("greetings", "Hello!");
+        prefEdit.commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        String msg = prefs.getString("greetings","No greetings name");
+        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
+
+
+    }
 }
